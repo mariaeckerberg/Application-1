a7b496c7b652c32c3290b9509974670d36aa9b56
Maria Eckerberg <maria@eckerberg.se>
2018-11-13 14:41:22 +0100

    Add DOM lab.

---
+++ labb.js
@@ -0,0 +1,21 @@
+﻿
+let el = document.querySelector("main div");
+console.log(el);
+
+el.addEventListener("click", function (event) {
+    setTimeout(function () {
+        console.log(event);
+        let newDiv = document.createElement("div");
+        el.appendChild(newDiv);
+        newDiv.innerHTML = "<strong>VERY IMPORTANT!!!!!</strong>"
+    }, 1000);
+});
+
+alert("Before");
+
+//overridear alert-funktionen så att det skrivs i konsollen istället:
+window.alert = function (message) {
+    console.log("ALERT!!!! " + message);
+};
+
+alert("After");
